a9a96a087a30b403ecccf0574a7fbee7373d4566
azoz1 <Abdulaziz.Alhothali.jed.k1@tuwaiq.edu.sa>
2022-04-13 11:27:20 +0300

    simple wordle game

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -21,27 +21,64 @@ class MainActivity : AppCompatActivity() {
             val row1 = listOf(et1,et2,et3,et4,et5)
             val row2 = listOf(et6,et7,et8,et9,et10)
             val row3 = listOf(et11,et12,et13,et14,et15)
+            val row4 = listOf(et16,et17,et18,et19,et20)
+            val row5 = listOf(et21,et22,et23,et24,et25)
+            val row6 = listOf(et26,et27,et28,et29,et30)
             button2.setOnClickListener {
-                    if (!et6.isEnabled && !et11.isEnabled ){
-                        checkMyWord("apple", row1)
+                when {
+                    !et6.isEnabled && !et11.isEnabled && !et16.isEnabled && !et21.isEnabled && !et26.isEnabled -> {
+                        checkMyWord("تفاحة", row1)
                         if (myb.contains(false)){
                             row2.forEach {
                                 it.isEnabled = true
                             }
                             myb.clear()
                         }
-                    } else if (!et11.isEnabled){
-                        checkMyWord("apple", row2)
+                    }
+                    !et11.isEnabled && !et16.isEnabled && !et21.isEnabled && !et26.isEnabled -> {
+                        checkMyWord("تفاحة", row2)
                         if (myb.contains(false)){
                             row3.forEach {
                                 it.isEnabled = true
                             }
-                            button2.isEnabled = true
                             myb.clear()
                         }
-                    } else {
-                        checkMyWord("apple", row3)
                     }
+                    !et16.isEnabled && !et21.isEnabled && !et26.isEnabled -> {
+                        checkMyWord("تفاحة", row3)
+                        if (myb.contains(false)){
+                            row4.forEach {
+                                it.isEnabled = true
+                            }
+                            myb.clear()
+                        }
+                    }
+                    !et21.isEnabled && !et26.isEnabled -> {
+                        checkMyWord("تفاحة", row4)
+                        if (myb.contains(false)){
+                            row5.forEach {
+                                it.isEnabled = true
+                            }
+                            myb.clear()
+                        }
+                    }
+                    !et26.isEnabled -> {
+                        checkMyWord("تفاحة", row5)
+                        if (myb.contains(false)){
+                            row6.forEach {
+                                it.isEnabled = true
+                            }
+                            myb.clear()
+                        }
+                    }
+                    else -> {
+                        checkMyWord("تفاحة", row6)
+                        if (button2.isEnabled){
+                            binding.button2.isEnabled = false
+                            Toast.makeText(this@MainActivity,"try again",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
             }
         }
 
@@ -50,13 +87,11 @@ class MainActivity : AppCompatActivity() {
     private fun checkMyWord(word: String, myWordEt: List<EditText>) {
         val myWord = myWordEt.joinToString("") { it.text.toString() }
         var i = 0
-        if (myWordEt[0].text.isNotEmpty() && myWordEt[1].text.isNotEmpty() && myWordEt[2].text.isNotEmpty() && myWordEt[3].text.isNotEmpty() && myWordEt[4].text.isNotEmpty()){
+        if (myWord.isNotEmpty()){
             myWordEt.forEach {
-                if (word.contains(it.text.toString()) && word[i] == myWord[i]) {
-                    myWordEt[i].setBackgroundColor(Color.GREEN)
-                }
-                if (word.contains(it.text.toString()) && word[i] != myWord[i]) myWordEt[i].setBackgroundColor(Color.BLUE)
-                if (!word.contains(it.text.toString())) myWordEt[i].setBackgroundColor(Color.GRAY)
+                if (word.contains(it.text.toString()) && word[i] == myWord[i]) it.setBackgroundColor(Color.GREEN)
+                if (word.contains(it.text.toString()) && word[i] != myWord[i]) it.setBackgroundColor(Color.BLUE)
+                if (!word.contains(it.text.toString())) it.setBackgroundColor(Color.GRAY)
                 i++
                 it.isEnabled = false
                 val c = (it.background as ColorDrawable).color
